38f8395d16172bab19ca896bc9c4c9171cf7c7b2
lgadrien <146069613+lgadrien@users.noreply.github.com>
2025-10-27 10:30:58 +0100

    Suppression du dossier de la V1

--- CryptoPilot-V1/api/routes/Prices.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPrices } from '../controller/controller.js';
-
-const router = express.Router();
-
-// Define the route to get prices
-router.get('/', getPrices);
-
-export default router;
--- CryptoPilot-V1/api/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import pricesRoutes from './Prices.js';
-
-const router = express.Router();
-
-// Use the prices routes
-router.use('/prices', pricesRoutes);
-
-export default router;
